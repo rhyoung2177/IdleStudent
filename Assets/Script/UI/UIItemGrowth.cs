83b38ee4ff1d6af939281a85dc1d8d1521124782
류하영 <81473599+rhyoung2177@users.noreply.github.com>
2021-08-25 23:15:31 +0900

    UI 정리

---
+++ Assets/Script/UI/UIItemGrowth.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using PolyAndCode.UI;
+
+public class UIItemGrowth : MonoBehaviour, ICell
+{
+    //Model
+    private ContactInfo _contactInfo;
+    private int _cellIndex;
+    //This is called from the SetCell method in DataSource
+    public void ConfigureCell(ContactInfo contactInfo, int cellIndex)
+    {
+        _cellIndex = cellIndex;
+        _contactInfo = contactInfo;
+    }
+}
